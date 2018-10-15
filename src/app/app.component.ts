be04fc4b803869f26425bea4236ca0c71d619524
chashchina_av <chashchina_av@taximaxim.local>
2018-10-15 12:00:59 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProjectsService } from './services/projects.service';
+import { Project } from './models/Project';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProjectsService]
 })
-export class AppComponent {
-  title = 'crowdin';
+export class AppComponent implements OnInit{
+  projects: Project[];
+  currentProject: Project;
+  constructor(private projectService: ProjectsService) {}
+
+  ngOnInit() {
+    this.projects = this.projectService.projects;
+  }
+
+  createNewProject() {
+    console.log(111)
+  }
+
+  getCurrentProject(currentProject: Project) {
+    this.currentProject = currentProject;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { MatIconModule, 
+         MatInputModule, 
+         MatButtonModule, 
+         MatButtonToggleModule, 
+         MatRadioModule,
+         MatSelectModule,
+         MatOptionModule, 
+         MatCheckboxModule,
+         MatTableModule} from '@angular/material'
 
 import { AppComponent } from './app.component';
+import { ProjectsService } from './services/projects.service';
+import { CurrentProjectSettingsComponent } from './current-project-settings/current-project-settings.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { LanguageService } from './services/languages.service';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'projects/:id',
+    component: CurrentProjectSettingsComponent
+  },
+  {
+    path: 'new-project',
+    component: NewProjectComponent
+  },
+  {
+    path: 'profile',
+    component: UserAccountComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentProjectSettingsComponent,
+    NewProjectComponent,
+    UserAccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    MatInputModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    FormsModule,
+    MatRadioModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ProjectsService, LanguageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
